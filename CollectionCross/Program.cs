90dddaedd4e8cb13b3e59fbc38235d6976b0072e
Сергей Усачев <usachevplayer@mail.ru>
2023-01-04 05:50:17 +0200

    final

--- CollectionCross/Program.cs
+++ CollectionCross/Program.cs
@@ -9,18 +9,13 @@
 
 			List<string> collection = new List<string>();
 
-			collection = MakingFilteredCollection(collection, arrayOne);
-			collection = MakingFilteredCollection(collection, arrayTwo);
+			MakeFilteredCollection(collection, arrayOne);
+			MakeFilteredCollection(collection, arrayTwo);
 
-			foreach (var item in collection)
+			ShowFilteredCollection(collection);
+
+			static void MakeFilteredCollection(List<string> collection, string[] array)
 			{
-				Console.Write(item);
-				Console.Write(" ");
-			}
-
-			static List<string> MakingFilteredCollection(List<string> collection, string[] array)
-			{
-
 				foreach (var item in array)
 				{
 					if (collection.Contains(item))
@@ -32,8 +27,15 @@
 						collection.Add(item);
 					}
 				}
+			}
 
-				return collection;
+			static void ShowFilteredCollection(List<string> collection)
+			{
+				foreach (var item in collection)
+				{
+					Console.Write(item);
+					Console.Write(" ");
+				}
 			}
 		}
 	}
